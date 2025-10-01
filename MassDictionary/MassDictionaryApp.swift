8d18dfd89ff9d773280fa0b28ad8b539b9e3b57a
Nabhan Rashid <nabhanbd06@gmail.com>
2025-10-01 15:23:03 -0400

    Initial Commit

---
+++ MassDictionary/MassDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MassDictionaryApp.swift
+//  MassDictionary
+//
+//  Created by Nabhan Rashid on 2025-10-01.
+//
+
+import SwiftUI
+
+@main
+struct MassDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
