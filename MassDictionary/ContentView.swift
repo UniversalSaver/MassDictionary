3f4ae59357ce1a5539fe434bef8f60ad8bd4b9b3
Nabhan Rashid <nabhanbd06@gmail.com>
2026-04-02 21:55:41 -0400

    Added a WordType to the TranslationBox. This will eventually tie in to making the app better at differentiating between different types of words.

--- MassDictionary/ContentView.swift
+++ MassDictionary/ContentView.swift
@@ -26,20 +26,22 @@ struct ContentView: View {
                 ForEach(0..<translations.count, id: \.description) { i in
                     Rectangle()
                         .path(in: CGRect(x: translations[i].x, y: translations[i].y, width: translations[i].width, height: translations[i].height))
-                        .stroke(Color.red, lineWidth: 4)
+                        .stroke(translations[i].getColor(), lineWidth: 4)
+                        .fill(translations[i].getColor().opacity(0.5))
                 }
 
             }
             
             Spacer()
+            
             Button("Show Image & Translate") {
                 withAnimation {
                     myImage = "Test Image"
                     
-                    translations.append(TranslationBox(x: 90, y: 335, width: 72, height: 40))
-                    translations.append(TranslationBox(x: 164, y: 335, width: 55, height: 40))
-                    translations.append(TranslationBox(x: 220, y: 335, width: 60, height: 40))
-                    translations.append(TranslationBox(x: 283, y: 335, width: 60, height: 40))
+                    translations.append(TranslationBox(x: 90, y: 335, width: 72, height: 40, type: WordType.noun))
+                    translations.append(TranslationBox(x: 164, y: 335, width: 55, height: 40, type: WordType.adjective))
+                    translations.append(TranslationBox(x: 220, y: 335, width: 60, height: 40, type: WordType.verb))
+                    translations.append(TranslationBox(x: 283, y: 335, width: 60, height: 40, type: WordType.particle))
                 }
                 
             }
--- MassDictionary/TranslationBox.swift
+++ MassDictionary/TranslationBox.swift
@@ -5,9 +5,29 @@
 //  Created by Nabhan Rashid on 2026-03-18.
 //
 
+import SwiftUI
+
+enum WordType {
+    case verb, noun, adjective, particle
+}
+
 struct TranslationBox {
     let x: Int
     let y: Int
     let width: Int
     let height: Int
+    let type: WordType
+    
+    func getColor() -> Color {
+        switch self.type {
+        case WordType.verb:
+            return Color.indigo
+        case WordType.noun:
+            return Color.blue
+        case WordType.adjective:
+            return Color.green
+        case WordType.particle:
+            return Color.pink
+        }
+    }
 }
