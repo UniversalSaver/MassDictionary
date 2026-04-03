0535b2d8d4295b75e802c2355304cb6e92f84bed
Nabhan Rashid <nabhanbd06@gmail.com>
2026-04-02 22:08:33 -0400

    Added a Features View, which gives a quick overview on what the app does.

---
+++ MassDictionary/FeatureCard.swift
@@ -0,0 +1,35 @@
+//
+//  FeaturesCard.swift
+//  MassDictionary
+//
+//  Created by Nabhan Rashid on 2026-04-02.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    let iconName: String
+    let description: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: iconName)
+                .font(.largeTitle)
+                .frame(width: 50)
+                .padding(.trailing, 10)
+                .border(.red, width: 2)
+            
+            Text(description)
+            
+            Spacer()
+        }
+        .padding()
+        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
+        .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    FeatureCard(iconName: "person.2.crop.square.stack.fill",
+                 description: "A multiline description about a feature paired with the image on the left.")
+}
